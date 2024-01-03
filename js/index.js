1dc55905ccb43970b6e3bd1cb9c85992a947459e
acrisanto <alanvcrisanto@gmail.com>
2024-01-03 09:29:11 -0500

    chore: modified header

--- js/index.js
+++ js/index.js
@@ -9,11 +9,6 @@ const isIndexPage = currentPagePath === "/index.html" || currentPagePath === "/"
 // variables de lazy loading
 const images = document.querySelectorAll(".img-lazy");
 
-// vaiables para mostrar resto de parrafo movil
-const btnMas = document.querySelector(".btnMore");
-const btnLess = document.querySelector(".btnLess");
-const showText = document.querySelector(".desplegar");
-
 // variables para generar lista de paises en select
 const select = document.querySelector(".section-select");
 
@@ -24,12 +19,6 @@ const sectionDiv6 = document.querySelectorAll(".section-input");
 // TABS INFO CURSO
 const tabs = document.querySelectorAll(".tab-btn");
 const contentTab = document.querySelectorAll(".content-tab");
-
-// Color border bottom docentes cards
-const docentesTitle = document.querySelectorAll(".card-content-title");
-
-// Alternar colores en card cursos
-const bgSlides = document.querySelectorAll(".card-8-desc");
 
 // Efector fade
 const sections = document.querySelectorAll(".fade-effect");
@@ -70,36 +59,11 @@ let animationObserver = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
 			entry.target.classList.add("fade-show");
-		} else {
-			entry.target.classList.remove("fade-show");
 		}
 	});
 });
 
 sections.forEach((el) => animationObserver.observe(el));
-
-// ALTERNAR COLORES EN CARDS
-function coloresCards() {
-	// const bgBlues = document.querySelectorAll(".section-1-text");
-	const bgSlides = document.querySelectorAll(".section-4-desc");
-
-	const bgGroup = [...bgSlides];
-
-	bgGroup.forEach((blue, index) => {
-		if (index % 3 === 0) {
-			// blue.style.backgroundColor = "rgba(34,44,63,0.7)"; // Color blue
-			blue.style.setProperty("--pseudo-element-color-main", "var(--blue)"); // Asignar color a pseudo elemento before
-		} else if (index % 3 === 1) {
-			// blue.style.backgroundColor = "rgba(251,192,9,0.5)"; // Color Yellow
-			blue.style.setProperty("--pseudo-element-color-main", "var(--main-yellow)"); // Asignar color a pseudo elemento before
-		} else {
-			// blue.style.backgroundColor = "rgba(222,29,39,0.7)"; // Color Red
-			blue.style.setProperty("--pseudo-element-color-main", "var(--red)"); // Asignar color a pseudo elemento before
-		}
-	});
-}
-
-// coloresCards();
 
 // SWIPER
 const swiperHeader = new Swiper(".swiperHeader", {
@@ -162,96 +126,6 @@ tabs.forEach((tab, index) => {
 		// Añade la clase active al content seleccionado
 		contentTab[index].classList.add("active");
 	});
-});
-
-// SWIPER
-// Docentes container 7
-const swiperDocentes = new Swiper(".swiperDocentes", {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	// centeredSlides: true,
-	// loop: true,
-	fade: true,
-	grabCursor: true,
-	// grid: {
-	// 	rows: 1,
-	// },
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	breakpoints: {
-		0: {
-			slidesPerView: 1,
-		},
-		520: {
-			slidesPerView: 2,
-		},
-		960: {
-			slidesPerView: 3,
-		},
-	},
-});
-
-// Cards cursos container 8
-const swiper = new Swiper(".mySwiper", {
-	loop: true,
-	effect: "coverflow",
-	grabCursor: true,
-	centeredSlides: true,
-	slidesPerView: 3,
-	coverflowEffect: {
-		rotate: 50,
-		stretch: 0,
-		depth: 100,
-		modifier: 1,
-		slideShadows: true,
-	},
-	breakpoints: {
-		0: {
-			slidesPerView: 1,
-		},
-		520: {
-			slidesPerView: 2,
-		},
-		960: {
-			slidesPerView: 3,
-		},
-	},
-	// Navigation arrows
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-});
-
-// Color Border-bottom docentes cards
-docentesTitle.forEach((tit, index) => {
-	if (index % 3 === 0) {
-		tit.style.borderBottom = "2px solid rgb(var(--blue)";
-	} else if (index % 3 === 1) {
-		tit.style.borderBottom = "2px solid rgb(var(--red)";
-	} else {
-		tit.style.borderBottom = "2px solid rgb(var(--main-yellow)";
-	}
-});
-
-// Alternar colores en cards cursos
-bgSlides.forEach((blue, index) => {
-	if (index % 3 === 0) {
-		// blue.style.backgroundColor = "rgba(34,44,63,0.7)"; // Color blue
-		blue.style.setProperty("--pseudo-element-color-main", "var(--blue)"); // Asignar color a pseudo elemento before
-	} else if (index % 3 === 1) {
-		// blue.style.backgroundColor = "rgba(251,192,9,0.5)"; // Color Yellow
-		blue.style.setProperty("--pseudo-element-color-main", "var(--main-yellow)"); // Asignar color a pseudo elemento before
-	} else {
-		// blue.style.backgroundColor = "rgba(222,29,39,0.7)"; // Color Red
-		blue.style.setProperty("--pseudo-element-color-main", "var(--red)"); // Asignar color a pseudo elemento before
-	}
 });
 
 // Copyright Year
